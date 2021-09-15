8784afcce722f15837a9d2ec5fa22560bb4ceff6
KangJuO <juy5437@gmail.com>
2021-09-15 11:53:03 +0900

    [add][reouter, controller]: 라우터, 컨트롤러 폴더 추가

--- EatMeUp-Server/index.js
+++ EatMeUp-Server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 80;
-const { sequelize } = require("./models");
+const { sequelize, Recipe } = require("./models");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,6 +21,35 @@ sequelize
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/test", async (req, res) => {
+  const test = await Recipe.findOne({ where: { post_user_id: 2 } });
+  console.log(test);
+  return res.json(test);
+});
+
+app.post("/qtest", async (req, res) => {
+  try {
+    const test = await Recipe.create({
+      title: "test",
+      description: "test",
+      main_image: "test",
+      foods: [
+        [null, "test"],
+        ["test", "test"],
+      ],
+      steps: [
+        ["test", "test"],
+        [null, "test"],
+      ],
+      post_user_id: 2,
+    });
+    console.log(test);
+    return res.json(test);
+  } catch (error) {
+    return res.json(error);
+  }
 });
 
 app.listen(port, () => {
