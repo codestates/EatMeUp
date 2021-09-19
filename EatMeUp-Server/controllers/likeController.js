46da3424dfa5984510a18907700609a672d4003c
KangJuO <juy5437@gmail.com>
2021-09-19 16:29:19 +0900

    [add][likeRecipe]: 레시피 좋아요 취소 추가

--- EatMeUp-Server/controllers/likeController.js
+++ EatMeUp-Server/controllers/likeController.js
@@ -6,9 +6,7 @@ const jwt = require("jsonwebtoken");
 const getLikeList = async (req, res) => {
   try {
     const user = jwt.verify(req.cookies.accessToken, config.accessSecret);
-
     const likeUser = await User.findOne({ where: { id: user.id } });
-
     const likeRecipe = await likeUser.getLikeRecipe();
 
     res.status(200).json({
@@ -42,7 +40,24 @@ const addLikeRecipe = async (req, res) => {
   }
 };
 
-const delLikeRecipe = async (req, res) => {};
+const delLikeRecipe = async (req, res) => {
+  try {
+    const user = jwt.verify(req.cookies.accessToken, config.accessSecret);
+    const likeUser = await User.findOne({ where: { id: user.id } });
+    const likeRecipe = await Recipe.findOne({ where: { id: req.body.id } });
+
+    await likeUser.removeLikeRecipe(likeRecipe);
+
+    res.status(201).json({
+      message: "remove success",
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "remove fail",
+      error: error,
+    });
+  }
+};
 
 module.exports = {
   delLikeRecipe,
