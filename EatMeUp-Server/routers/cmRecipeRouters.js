2597974ef840f1842ed4acb3a3300e2469192d18
wookieOH <ehddnrdh@gmail.com>
2021-09-29 21:59:14 +0900

    [mod][server] recipe get_post

--- EatMeUp-Server/routers/cmRecipeRouters.js
+++ EatMeUp-Server/routers/cmRecipeRouters.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const {
-  getRecipe,
-  getFoodRecipe,
+  postRecipe,
+  postFoodRecipe,
   getRecipeDetail,
 } = require("../controllers/cmRecipeController");
 // const { auth } = require("../utils/checkAuth");
 
 const cmRecipeRouter = express.Router();
 
-cmRecipeRouter.route("/info").get(getRecipe);
-cmRecipeRouter.route("/food").get(getFoodRecipe);
+cmRecipeRouter.route("/info").post(postRecipe);
+cmRecipeRouter.route("/food").post(postFoodRecipe);
 cmRecipeRouter.route("/info/:id").get(getRecipeDetail);
 
 module.exports = cmRecipeRouter;
